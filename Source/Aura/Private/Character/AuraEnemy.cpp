98959481fedd5e9c670cafc6d26fd08b9253fe24
chris-g-selch <c.gholston1861@gmail.com>
2023-07-29 16:34:09 -0500

    Postproccess Volume with material set to unbounded has been added to project and file structure has been changed

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,43 @@
 
 #include "Character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
+//Required Post Processing Volume
+//Post Processing Highlight material
+//For this to work
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
+
 void AAuraEnemy::HighlightActor()
 {
-	DrawDebugSphere(GetWorld(),GetActorLocation(),50.f,12,FColor::Blue, false, .5f);
+	if(USkeletalMeshComponent* cachedMesh = GetMesh())
+	{
+		cachedMesh->SetRenderCustomDepth(true);
+		cachedMesh->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);	
+	}
+
+	if(Weapon)
+	{
+		Weapon->SetRenderCustomDepth(true);
+		Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	}
+	
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	DrawDebugSphere(GetWorld(),GetActorLocation(),12.f,12,FColor::Red,false,.5f);
+	if(USkeletalMeshComponent* cachedMesh = GetMesh())
+	{
+		cachedMesh->SetRenderCustomDepth(false);
+	}
+
+	if(Weapon)
+	{
+		Weapon->SetRenderCustomDepth(false);
+	}
+
 }
